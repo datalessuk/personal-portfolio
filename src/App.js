06b1a490b8091d3e6370ca25045862c84b071fcd
datalessuk <dataless@gmail.com>
2021-05-17 15:14:05 +0100

    icon changes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App(props) {
       
 
 
-      <Projects title="Personal Portfolio" mainHeading={"Projects"} subHeading={"Small collection of my projects"}  image={personalImage} info="My Personal Portfolio made with React with Vanilla HTML5 and CSS/SASS."infoSecondLine='Chat bot is build with the React-chatbot-kit, with the Openweathermap API for live weather stats' link={'#'}/>
+      <Projects title="Personal Portfolio" mainHeading={"Projects"} subHeading={"Small collection of my projects"}  image={personalImage} info="My Personal Portfolio made with React with Vanilla HTML5 and CSS/SASS."infoSecondLine='Chat bot is build with the React-chatbot-kit, with the Openweathermap API for live weather stats' link={'https://github.com/datalessuk/personal-portfolio'}/>
       <Projects title="When to wake up" image={whenToWake} info="An app that calculates the perfect time to wake up in the morning feeling well rested and ready for the day. Based on the average time it takes to fall asleep" link={'#'}/>
       
       
