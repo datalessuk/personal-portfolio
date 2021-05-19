bb65d8037a09486aa77980f823993920de973b82
datalessuk <dataless@gmail.com>
2021-05-19 11:47:44 +0100

    mobile view / header

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import MessageParser from '../src/components/chatBot/MessageParser';
 
 import ChatBot from 'react-simple-chatbot';
 
-//
+
 
 function App(props) {
 
@@ -29,16 +29,7 @@ function App(props) {
   return (
     <div className="App">
       <Header />
-      <AboutMe />
-      <ChatButton />
-      
-
-
-      <Projects title="Personal Portfolio" mainHeading={"Projects"} subHeading={"Small collection of my projects"}  image={personalImage} info="My Personal Portfolio made with React with Vanilla HTML5 and CSS/SASS."infoSecondLine='Chat bot is build with the React-chatbot-kit, with the Openweathermap API for live weather stats' link={'https://github.com/datalessuk/personal-portfolio'}/>
-      <Projects title="When to wake up" image={whenToWake} info="An app that calculates the perfect time to wake up in the morning feeling well rested and ready for the day. Based on the average time it takes to fall asleep" link={'#'}/>
-      
-      
-      <Footer />
+     
       
     </div>
   );
