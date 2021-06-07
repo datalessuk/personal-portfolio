eb3f59bbadb28a5d86d10f0e951b0319be8897bc
datalessuk <dataless@gmail.com>
2021-06-07 20:43:36 +0100

    heading spacing

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -6,7 +6,7 @@ function WelcomeText(){
             <h5>Mark <span>Harrison</span></h5>
             <br />
             <h4 id="main-heading"><span></span>Junior Software Engineer</h4>
-            <h5><span className="map-icon"><img src={mapMarker} alt="mapmarker"></img></span>United kingdom</h5>
+            <h5><span className="map-icon"><img src={mapMarker} alt="mapmarker"></img></span> United kingdom</h5>
             </div>
     )
 }
