80a81cf10975b3592ea0500849105345427b552b
datalessuk <dataless@gmail.com>
2021-06-30 13:28:06 +0100

    contact form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Header from './components/Header';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+import ContactForm from './components/ContactMe';
 import ChatButton from './components/ChatButton'
+
+
 
 
 
@@ -18,9 +21,9 @@ function App(props) {
       <Header />
       <AboutMe />
       <Projects title="Personal Portfolio" mainHeading={"Projects"} subHeading={"Small collection of my projects"}  image={personalImage} info="This is my personal portfolio of work, it was made using Vanilla HTML5, the React framework and Sass."infoSecondLine='Chat bot is build with the React-chatbot-kit, with the Openweathermap API for live weather stats that shows the live weather in London.' link={'https://github.com/datalessuk/personal-portfolio'} active={'disabled-link'}/>
-      
       <Projects title="When to wake up"
        image={whenToWake} info="An app that calculates the perfect time to wake up in the morning feeling well rested and ready for the day. Based on the average time it takes to fall asleep" link={'https://github.com/datalessuk/whentowakeup'} active={'enabled-link'} liveView={'https://gallant-mccarthy-fb8244.netlify.app/'}/>
+      <ContactForm />
       <ChatButton />
       <Footer />
     </div>
---
+++ src/components/ContactMe.js
@@ -0,0 +1,44 @@
+import '../styles/Contact.scss';
+import emailjs from 'emailjs-com';
+import React from 'react';
+
+
+export default function ContactMe(){
+
+
+    
+        function sendEmail(e){
+            e.preventDefault();
+
+           emailjs.sendForm('service_28zam0o', 'template_03yridi', e.target, 'user_fia3MXBLdB6QY75QEyvUR')
+             .then((result) => {
+                 console.log(result.text);
+             }, (error) => {
+                 console.log(error.text);
+             });
+           e.target.reset();//resets the form 
+        }
+        
+           
+           return(
+               
+               <div className="email-container">
+                   <div className="heading"><h1>Contact Me</h1></div>
+                   <form className="contact-form" onSubmit={sendEmail}>
+                   <input type="text" placeholder="Name" id="text-input" name="from_name"/>
+                   <br />
+                   <input type="text" placeholder="Email Address" id="text-input" name="email"/>
+                   <br />
+                   <input type="text" placeholder="Subject" id="text-input" name="subject"/>
+                   <br />
+                   <textarea type="text" placeholder="Message" id="message-input" name="message"/>
+                   <br />
+                   <div className="button">
+                   <input type="submit" value="Send Message"/>
+                   </div>
+                   
+                   
+                   </form>
+               </div>
+           )
+       }
